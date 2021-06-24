571b85e2122aaad28b58784dfa3127c55441e377
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-06-24 18:47:38 -0400

    add sessions controler for user login, add conditional html for user login/logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to '/'
     else
       render :new
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
 
   resources :about, only: [:index]
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
