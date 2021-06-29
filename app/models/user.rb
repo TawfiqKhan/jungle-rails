1c45e6b44a43b9160adc1c245fc7bf163726b816
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-06-28 21:49:15 -0400

    add tests for user authentications, update user model to create new authentication function

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,10 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :password_digest, presence: true
+  validates :password, presence: true, length: { minimum: 5 }
+
+  def self.authenticate_with_credentials email, password
+    user = User.find_by_email(email.downcase.strip)
+    user && user.authenticate(password) ? user : nil
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "User Authentication" do
-
+  describe "User Validation" do
+  # .new creates but doesnt save in DB, .creates creates and saves
   it "Saves a valid user" do
     @user = User.new(first_name: "A", last_name: "B", email: "test@gmail.com", password: "abc123", password_confirmation: "abc123")
     expect(@user).to be_valid
@@ -18,5 +18,56 @@ RSpec.describe User, type: :model do
     @user2 = User.create(first_name: "A", last_name: "B", email: "test@gmail.com", password: "abc123", password_confirmation: "abc123")
     expect(@user2.errors.full_messages[0]).to eq ("Email has already been taken")
   end
+
+  it "Returns errors if email already exists" do
+    @user = User.create(first_name: "A", last_name: "B", email: "test@gmail.com", password: "abc123", password_confirmation: "abc123")
+    @user2 = User.create(first_name: "A", last_name: "B", email: "test@gmail.com", password: "abc123", password_confirmation: "abc123")
+    expect(@user2.errors.full_messages[0]).to eq ("Email has already been taken")
+  end
+
+  it "Validates first name" do
+    @user = User.create(first_name: nil, last_name: "B", email: "test@gmail.com", password: "abc123", password_confirmation: "abc123")
+    expect(@user.errors.full_messages[0]).to eq ("First name can't be blank")
+  end
+
+  it "validates last name" do
+    @user = User.create(first_name: "A", last_name: nil, email: "test@gmail.com", password: "abc123", password_confirmation: "abc123")
+    expect(@user.errors.full_messages[0]).to eq ("Last name can't be blank")
+  end
+
+  it "validates email" do
+    @user = User.create(first_name: "A", last_name: "B", email: nil, password: "abc123", password_confirmation: "abc123")
+    expect(@user.errors.full_messages[0]).to eq ("Email can't be blank")
+  end
+
+  it "Returns error if provided password length less than minimum" do
+    @user = User.create(first_name: "A", last_name: "B", email: "test@gmail.com", password: "abc", password_confirmation: "abc")
+    expect(@user.errors.full_messages[0]).to eq ("Password is too short (minimum is 5 characters)")
+  end
+
 end
+
+describe "User Authentication" do
+  it "Allows Login if password is correct" do
+  @user = User.create(first_name: "A", last_name: "B", email: "test2@gmail.com", password: "abcde", password_confirmation: "abcde")
+  expect(User.authenticate_with_credentials("test2@gmail.com","abcde")).to eq(@user)
+  end
+  
+  it "Retruns error if password mismatch" do
+  @user = User.create(first_name: "A", last_name: "B", email: "test2@gmail.com", password: "abcde", password_confirmation: "abcdef")
+  expect(User.authenticate_with_credentials("test2@gmail.com","abcde")).to eq(nil)
+  end
+  
+  it "Allows login if email has whitespaces" do
+  @user = User.create(first_name: "A", last_name: "B", email: "test2@gmail.com", password: "abcde", password_confirmation: "abcde")
+  expect(User.authenticate_with_credentials("  test2@gmail.com  ","abcde")).to eq(@user)
+  end
+  
+  it "Allows login if email has uppercase letters" do
+  @user = User.create(first_name: "A", last_name: "B", email: "test2@gmail.com", password: "abcde", password_confirmation: "abcde")
+  expect(User.authenticate_with_credentials("TesT2@gMail.com","abcde")).to eq(@user)
+  end
+
+end
+
 end
